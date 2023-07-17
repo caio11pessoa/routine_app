a79f1501379e5f2ac0dc76f840f7ab20a04eb5b3
Caio Pessoa <caio.pessoa@institutoiracema.org.br>
2023-07-17 15:18:22 -0300

    First commit

---
+++ lib/collections/routine.dart
@@ -0,0 +1,17 @@
+import 'package:isar/isar.dart';
+import 'package:routine_app/collections/category.dart';
+part 'routine.g.dart';
+
+@Collection()
+class Routine {
+  Id id = Isar.autoIncrement;
+  late String title;
+  @Index()
+  late DateTime startTime;
+
+  @Index(caseSensitive: false)
+  late String day;
+
+  @Index(composite: [CompositeIndex('title')])
+  final category = IsarLink<Category>();
+}
